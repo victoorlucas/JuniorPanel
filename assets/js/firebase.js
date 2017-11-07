9dc670edf53e47a67e9210607b062e12af6f84f3
Victor Lucas <victorlucas8@hotmail.com>
2017-11-07 12:49:27 -0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,10 +1,10 @@
 var config = {
-    apiKey: "AIzaSyDumdbGv7uo7LsIJ5WWoWwP2zbxgVoZNz0",
-    authDomain: "admtechsoft.firebaseapp.com",
-    databaseURL: "https://admtechsoft.firebaseio.com",
-    projectId: "admtechsoft",
+    apiKey: "APIKEY",
+    authDomain: "AUTHDOMAIN",
+    databaseURL: "DATABASEURL",
+    projectId: "PROJECTID",
     storageBucket: "",
-    messagingSenderId: "140194762510"
+    messagingSenderId: "MESSAGINGSENDERID"
   };
   firebase.initializeApp(config);
 
